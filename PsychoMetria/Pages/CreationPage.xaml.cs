675131f0b02203c0f6038763390734039f1c2cb2
Anhalt-River <85508867+Anhalt-River@users.noreply.github.com>
2024-06-03 18:34:40 +0300

    3.06.24/18:34 - Закончен интерфейс создания теста. Надлежит разработать интерфейс доп.окон для работы с вопросами и шкалами

--- PsychoMetria/Pages/CreationPage.xaml.cs
+++ PsychoMetria/Pages/CreationPage.xaml.cs
@@ -35,6 +35,7 @@ namespace PsychoMetria.Pages
             KZA.Add("sdsdsds");
             KZA.Add("sdsdsds");
             ScalesList.ItemsSource = KZA;
+            QuestionsList.ItemsSource = KZA;
         }
 
         private void BackToMainBut_Click(object sender, RoutedEventArgs e)
@@ -43,6 +44,11 @@ namespace PsychoMetria.Pages
         }
 
         private void CreateQuestionnaireBut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ScaleBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
@@ -57,24 +63,131 @@ namespace PsychoMetria.Pages
 
         }
 
-        private void ScaleBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void AddNewQuestionBut_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void EditBut_Click(object sender, RoutedEventArgs e)
+        private void EditQuestionBut_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void DeleteBut_Click(object sender, RoutedEventArgs e)
+        private void DeleteQuestionBut_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        private void UpQuestionBut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DownQuestionBut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Функции для Шкал
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
         private void AddNewScaleBut_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void EditScaleBut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteScaleBut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Перемешать вопросы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        bool _isMixUp = false;
+        private void MixUpQuestionsCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            _isMixUp = true;
+
+            App.Current.Resources["DefaultStackPanel"] = App.Current.Resources["UnvisibleStackPanel"];
+        }
+
+        private void MixUpQuestionsCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _isMixUp = false;
+            App.Current.Resources["DefaultStackPanel"] = App.Current.Resources["VisibleStackPanel"];
+        }
+
+        /// <summary>
+        /// Режимы скрытия и раскрытия информации
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        bool _isMainInfoOpen = false;
+        private void MainInfoVisibilityBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isMainInfoOpen)
+            {
+                MainInfoPanel.Visibility = Visibility.Collapsed;
+                _isMainInfoOpen = false;
+                MainInfoVisibilityText.Text = "(+)";
+                MainInfoVisibilityBut.ToolTip = "Раскрыть основную информацию";
+            }
+            else
+            {
+                MainInfoPanel.Visibility = Visibility.Visible;
+                _isMainInfoOpen = true;
+                MainInfoVisibilityText.Text = "(-)";
+                MainInfoVisibilityBut.ToolTip = "Скрыть основную информацию";
+            }
+        }
+
+        bool _isScalesOpen = false;
+        private void ScalesVisibilityBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isScalesOpen)
+            {
+                ScalesPanel.Visibility = Visibility.Collapsed;
+                _isScalesOpen = false;
+                ScalesVisibilityText.Text = "(+)";
+                ScalesVisibilityBut.ToolTip = "Раскрыть информацию о предмете тестирования";
+            }
+            else
+            {
+                ScalesPanel.Visibility = Visibility.Visible;
+                _isScalesOpen = true;
+                ScalesVisibilityText.Text = "(-)";
+                ScalesVisibilityBut.ToolTip = "Скрыть информацию о предмете тестирования";
+            }
+        }
+
+        bool _isQuestionsOpen = false;
+        private void QuestionsVisibilityBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isQuestionsOpen)
+            {
+                QuestionsPanel.Visibility = Visibility.Collapsed;
+                _isQuestionsOpen = false;
+                QuestionsVisibilityText.Text = "(+)";
+                QuestionsVisibilityBut.ToolTip = "Раскрыть информацию о вопросах тестирования";
+            }
+            else
+            {
+                QuestionsPanel.Visibility = Visibility.Visible;
+                _isQuestionsOpen = true;
+                QuestionsVisibilityText.Text = "(-)";
+                QuestionsVisibilityBut.ToolTip = "Скрыть информацию о вопросах тестирования";
+            }
         }
     }
 }
